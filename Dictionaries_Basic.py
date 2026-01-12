d23b5d894cea5398d5fe9dc0115c869ce96894f7
PoonamKadam1 <autade.poonam@gmail.com>
2026-01-12 13:56:10 +0000

    Add files via upload
    Add Dictionaries_Basic.py

---
+++ Dictionaries_Basic.py
@@ -0,0 +1,10 @@
+Student_Details = { "Name": 'Poonam' ,
+                    "Age" :30,
+                    "School" :"MSc Computer science"
+
+}
+print (Student_Details["Name"])
+print (Student_Details["Age"])
+Student_Details["Age"]= 28
+Student_Details["university"]= "Reykjavik university"
+print (Student_Details)
